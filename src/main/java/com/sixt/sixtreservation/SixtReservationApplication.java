8d3751eccab1c387cfde6ffa70ef7fc1924ee78d
ValentinFLGT <valentin.folliguet@le-campu-numerique.fr>
2021-01-27 09:23:55 +0100

    Initial commit

---
+++ src/main/java/com/sixt/sixtreservation/SixtReservationApplication.java
@@ -0,0 +1,13 @@
+package com.sixt.sixtreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixtReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixtReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixt/sixtreservation/SixtReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixt.sixtreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixtReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
